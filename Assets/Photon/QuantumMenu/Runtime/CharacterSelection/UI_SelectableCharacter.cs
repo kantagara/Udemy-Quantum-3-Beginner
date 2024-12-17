7cc5690286add7a406840c9404e30ad1286486ce
Nikola Garabandic <kantagara@gmail.com>
2024-12-17 18:44:12 +0100

    34 - Character Selection Part 2 - UI Representation of Character Model, Selecting the Character

---
+++ Assets/Photon/QuantumMenu/Runtime/CharacterSelection/UI_SelectableCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Quantum.Menu.CharacterSelection
+{
+    public class UI_SelectableCharacter : MonoBehaviour
+    {
+        [SerializeField] private Image characterImage;
+        [SerializeField] private TMP_Text characterName;
+        [SerializeField] private GameObject characterSelected;
+
+        private CharacterModel _model;
+
+        public static event Action<CharacterModel> OnCharacterSelected; 
+
+        public void Initialize(CharacterModel model)
+        {
+            characterImage.sprite = model.CharacterImage;
+            characterName.text = model.CharacterName;
+            _model = model;
+        }
+
+        public void CharacterSelected()
+        {
+            OnCharacterSelected?.Invoke(_model);
+        }
+
+        public void SetSelected(bool isSelected)
+        {
+            characterSelected.SetActive(isSelected);
+        }
+    }
+}
--- Assets/Photon/QuantumMenu/Runtime/QuantumMenuUICharacterSelection.cs
+++ Assets/Photon/QuantumMenu/Runtime/QuantumMenuUICharacterSelection.cs
@@ -1,15 +1,57 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Quantum.Menu.CharacterSelection;
+using UnityEngine;
 
 namespace Quantum.Menu
 {
     public class QuantumMenuUICharacterSelection : QuantumMenuUIScreen
     {
         [SerializeField] private CharacterModel[] characterModels;
+        [SerializeField] private UI_SelectableCharacter selectableCharacter;
+        [SerializeField] private Transform characterSelectionParent;
+        [SerializeField] private QuantumMenuUIController quantumMenuUIController;
+        private Dictionary<CharacterModel, UI_SelectableCharacter> _selectableCharacterMap = new ();
+        private CharacterModel _characterModelCurrentlySelected;
+        
         public override void Awake()
         {
-            
+            UI_SelectableCharacter.OnCharacterSelected += CharacterSelected;
+            InitalizeCharacterSelection();
         }
-        
+
+        private void InitalizeCharacterSelection()
+        {
+            for (int i = 0; i < characterModels.Length; i++)
+            {
+                var characterModel = characterModels[i];
+                var uiSelectableCharacter = Instantiate(selectableCharacter, characterSelectionParent);
+                uiSelectableCharacter.Initialize(characterModel);
+                _selectableCharacterMap.Add(characterModel, uiSelectableCharacter);
+            }
+            
+            CharacterSelected(characterModels[0]);
+        }
+
+        private void OnDestroy()
+        {
+            UI_SelectableCharacter.OnCharacterSelected -= CharacterSelected;
+        }
+
+        private void CharacterSelected(CharacterModel model)
+        {
+            if(_characterModelCurrentlySelected == model)
+                return;
+            if (_characterModelCurrentlySelected != null)
+            {
+                _selectableCharacterMap[_characterModelCurrentlySelected].SetSelected(false);
+            }
+            
+            _selectableCharacterMap[model].SetSelected(true);
+            _characterModelCurrentlySelected = model;
+            quantumMenuUIController.ConnectArgs.RuntimePlayers[0].PlayerAvatar = model.EntityPrototype;
+        }
+
         /// <summary>
         /// Is called when the <see cref="_backButton"/> is pressed using SendMessage() from the UI object.
         /// </summary>
