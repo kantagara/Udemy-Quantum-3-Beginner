e0e9893c72330ede164ec708be4c8fc051db8a16
Nikola Garabandic <kantagara@gmail.com>
2024-12-17 07:49:07 +0100

    14 - Moving The Bullets, Bullet Collision Detection

--- Assets/QuantumUser/Simulation/Systems/BulletSystem.cs
+++ Assets/QuantumUser/Simulation/Systems/BulletSystem.cs
@@ -5,12 +5,56 @@ namespace Quantum {
 
   [Preserve]
   public unsafe class BulletSystem : SystemMainThreadFilter<BulletSystem.Filter>, ISignalCreateBullet {
-    public override void Update(Frame f, ref Filter filter) {
+    public override void Update(Frame f, ref Filter filter)
+    {
+      var nextPosition = filter.Bullet->Direction * filter.Bullet->Speed * f.DeltaTime;
+
+      if (CheckForCollisions(f, filter, nextPosition, out var entityHit))
+      {
+        f.Destroy(filter.Entity);
+        return;
+      }
+
+      CheckBulletForTimeExpiration(f, filter);
+      
+      
+      filter.Transform->Position += nextPosition;
+    }
+
+    private void CheckBulletForTimeExpiration(Frame frame, Filter filter)
+    {
+      filter.Bullet->Time -= frame.DeltaTime;
+      if (filter.Bullet->Time <= 0)
+      {
+        frame.Destroy(filter.Entity);
+      }
+    }
+
+    private bool CheckForCollisions(Frame frame, Filter filter, FPVector2 nextPosition, out EntityRef entityHit)
+    {
+      entityHit = EntityRef.None;
+
+      var owner = filter.Bullet->Owner;
+      var bulletTransform = frame.Get<Transform2D>(filter.Entity);
+      var collisions = frame.Physics2D.LinecastAll(bulletTransform.Position, bulletTransform.Position + nextPosition,
+        int.MaxValue, QueryOptions.HitAll & ~QueryOptions.HitTriggers);
+
+      for (var i = 0; i < collisions.Count; i++)
+      {
+        var collision = collisions[i];
+        if(collision.Entity == filter.Entity || collision.Entity == owner)
+          continue;
+        entityHit = collision.Entity;
+        return true;
+      }
+      
+      return false;
     }
 
     public struct Filter {
       public EntityRef Entity;
       public Bullet* Bullet;
+      public Transform2D* Transform;
     }
 
     public void CreateBullet(Frame f, EntityRef owner, WeaponData weaponData)
@@ -25,6 +69,7 @@ namespace Quantum {
       var bullet = f.Unsafe.GetPointer<Bullet>(bulletEntity);
       bullet->Speed = bulletData.Speed;
       bullet->Damage = bulletData.Damage;
+      bullet->HeightOffset = weaponData.Offset.Y;
       bullet->Owner = owner;
       bullet->Time = bulletData.Duration;
       bullet->Direction = ownersTransform.Up;
