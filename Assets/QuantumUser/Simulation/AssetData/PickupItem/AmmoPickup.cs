bf53916de4b228d7bf2d25743ff30ac8d57e866f
Nikola Garabandic <kantagara@gmail.com>
2024-12-17 15:38:39 +0100

    28 - Ammo Pickup

---
+++ Assets/QuantumUser/Simulation/AssetData/PickupItem/AmmoPickup.cs
@@ -0,0 +1,16 @@
+﻿namespace Quantum
+{
+    public unsafe class AmmoPickup : PickupItemBase
+    {
+        public override void PickupItem(Frame f, EntityRef entityBeingPickedUp, EntityRef entityPickingUp)
+        {
+            var weapon = f.Unsafe.GetPointer<Weapon>(entityPickingUp);
+            var weaponData = f.FindAsset<WeaponBase>(weapon->WeaponData);
+            if(weaponData is not FiringWeapon firingWeapon)
+                return;
+            weapon->Ammo = firingWeapon.MaxAmmo;
+            f.Events.AmmoChanged(entityPickingUp, weapon->Ammo);
+            f.Destroy(entityBeingPickedUp);
+        }
+    }
+}
